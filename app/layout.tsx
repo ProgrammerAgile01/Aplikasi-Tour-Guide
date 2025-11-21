92bd7578fd3d180d074a56ee1acd171e20770aab
Programmer Agile 02 <programmeragile02@gmail.com>
2025-11-21 10:24:46 +0700

    memperbaiki package.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Pemandu Wisata Eksekutif",
-  description: "Aplikasi panduan wisata eksklusif untuk eksekutif",
+  title: "Teman Wisata",
+  description: "Aplikasi pemandu wisata eksekutif",
   icons: {
     icon: [
       {
